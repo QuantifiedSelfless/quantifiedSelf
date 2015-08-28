5276be5d3956a63e34dde69d60f8c96d2ed7570c
wannabeCitizen <doorsofskirpan@gmail.com>
2015-08-28 03:18:51 +0000

    base server and requirements file set up for scraping

--- test_serve.py
+++ test_serve.py
@@ -3,13 +3,13 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Here to help you figure out reality since 1872.")
 
 app = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
-    app.listen(80)
+    app.listen(6060)
     tornado.ioloop.IOLoop.current().start()
 

cf1db821df34dfbddfddbbe75e5b44827281415b
Slaton <slsp8752@colorado.edu>
2015-08-31 21:08:42 +0000

    Twitter user data now gets inserted into rethinkdb

--- twitter/twitter_scrape.py
+++ twitter/twitter_scrape.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
+import rethinkdb as r
 from TwitterAPI import TwitterAPI
 from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
+
+
+conn = r.connect(host='localhost', port=28015, db='Twitter')
+
+conn.repl()
 
 api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 screen_name = raw_input("Enter a Screen Name: ")
 
-info = api.request('users/show', {'screen_name':screen_name})
+info_raw = api.request('users/show', {'screen_name':screen_name})
+info_dict = {}
 
-for item in info:
+for item in info_raw:
 	for entry in item:
 		if str(entry) != 'status':
-			print(str(entry), str(item[entry]))
+			info_dict[str(entry)] = str(item[entry])
 
+r.table("Users").insert([info_dict]).run()
 

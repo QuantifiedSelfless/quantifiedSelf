15dbdb4a0dbb184348f20a40850845059e6952b3
slsp8752 <slsp8752@colorado.edu>
2015-09-07 01:42:31 -0400

    progress on friends, in need of restructure

--- twitter/twitter_scrape.py
+++ twitter/twitter_scrape.py
@@ -3,6 +3,7 @@
 # TODO
 # Delve into user relations
 #	-change into function, recursively call
+#	-Depth first search: scrape friends, then friends of friends
 
 import rethinkdb as r
 from TwitterAPI import TwitterAPI
@@ -16,16 +17,29 @@ api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 screen_name = raw_input("Enter a Screen Name: ")
 
+get_user_data(screen_name)
+
 # Start Function Here
-# def get_user_data():
+def get_user_data(username):
 
-friends = api.request('friends/ids', {'screen_name':screen_name})
-info_raw = api.request('users/show', {'screen_name':screen_name})
-info_dict = {}
+	friend_list = api.request('friends/list', {'screen_name':screen_name})
+	info_raw = api.request('users/show', {'screen_name':screen_name})
+	info_dict = {}
 
-for item in info_raw:
-	for entry in item:
-		if str(entry) != 'status':
-			info_dict[str(entry)] = str(item[entry])
+	for item in info_raw:
+		for entry in item:
+			if str(entry) != 'status':
+				info_dict[entry] = item[entry]
 
-r.table("Users").insert([info_dict],conflict="update").run()
+	friend_count = info_dict['friends_count']
+	friend_info_array = [dict() for x in range(friend_count)]
+	r.table("Users").insert([info_dict],conflict="update").run()
+
+	# Rework - queue of ids
+	# tweets, friends, favorites per each person
+	# iterative instead of recursive
+
+	for dictionary in friend_info_array:
+		for person in friend_list:
+			for category in person:
+				dictionary[category] = person[category]

ba6ee71bbb2f94d0d6eefa8e4a103fb7b99b7505
slsp8752 <slsp8752@colorado.edu>
2015-09-06 22:25:20 -0400

    Added TODO, started recursive exploration of users

--- twitter/twitter_scrape.py
+++ twitter/twitter_scrape.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+# TODO
+# Delve into user relations
+#	-change into function, recursively call
+
 import rethinkdb as r
 from TwitterAPI import TwitterAPI
 from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
-
 
 conn = r.connect(host='localhost', port=28015, db='Twitter')
 
@@ -13,6 +16,10 @@ api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 screen_name = raw_input("Enter a Screen Name: ")
 
+# Start Function Here
+# def get_user_data():
+
+friends = api.request('friends/ids', {'screen_name':screen_name})
 info_raw = api.request('users/show', {'screen_name':screen_name})
 info_dict = {}
 
@@ -21,5 +28,4 @@ for item in info_raw:
 		if str(entry) != 'status':
 			info_dict[str(entry)] = str(item[entry])
 
-r.table("Users").insert([info_dict]).run()
-
+r.table("Users").insert([info_dict],conflict="update").run()

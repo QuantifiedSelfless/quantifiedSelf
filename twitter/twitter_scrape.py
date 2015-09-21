4cfea13694543c300f46ce4f3dc0ed07837e5505
slsp8752 <slsp8752@colorado.edu>
2015-09-20 21:03:48 -0400

    Friend info is now stored in database per user, tweet data needs tweaking.

--- twitter/twitter_scrape.py
+++ twitter/twitter_scrape.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python
 
 # TODO
-# Delve into user relations
-#	-change into function, recursively call
-#	-Depth first search: scrape friends, then friends of friends
+# Shrink friend data down?
+# For each friend, 
 
 import rethinkdb as r
 from TwitterAPI import TwitterAPI
 from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
+
+# Start Function Here
+
+def parse_user_info(user_objects):
+	user_dict = {}
+	dict_dict = {}
+	item_count = 0
+	for item in user_objects:
+		item_count += 1
+		for entry in item:
+			if str(entry) == 'name' or str(entry) == 'screen_name' or str(entry) == 'followers_count':
+				if(item_count > 1):
+					dict_dict[item_count-1] = str(user_dict)
+				user_dict[entry] = item[entry]
+	if item_count > 1:
+		return dict_dict
+	else:
+		return user_dict
+
+def get_user_data(username):
+        friend_list = api.request('friends/list', {'screen_name':username})
+        info_raw = api.request('users/show', {'screen_name':username})
+        tweets = api.request('statuses/user_timeline', {'screen_name':username, 'count':'200', 'exclude_replies':'true'})
+        
+	info_dict = parse_user_info(info_raw)
+
+	friend_dict = parse_user_info(friend_list)
+
+        r.table("Users").insert([info_dict],conflict="update").run()
+
+	r.table("Users").filter({"screen_name":username}).update({"friends":str(friend_dict)},conflict="update").run()
+
+	r.table("Users").filter({'screen_name':username}).update({"tweets":str(tweets)},conflict="update").run()
+	return 0
 
 conn = r.connect(host='localhost', port=28015, db='Twitter')
 
@@ -18,28 +51,3 @@ api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 screen_name = raw_input("Enter a Screen Name: ")
 
 get_user_data(screen_name)
-
-# Start Function Here
-def get_user_data(username):
-
-	friend_list = api.request('friends/list', {'screen_name':screen_name})
-	info_raw = api.request('users/show', {'screen_name':screen_name})
-	info_dict = {}
-
-	for item in info_raw:
-		for entry in item:
-			if str(entry) != 'status':
-				info_dict[entry] = item[entry]
-
-	friend_count = info_dict['friends_count']
-	friend_info_array = [dict() for x in range(friend_count)]
-	r.table("Users").insert([info_dict],conflict="update").run()
-
-	# Rework - queue of ids
-	# tweets, friends, favorites per each person
-	# iterative instead of recursive
-
-	for dictionary in friend_info_array:
-		for person in friend_list:
-			for category in person:
-				dictionary[category] = person[category]

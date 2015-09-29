7f36f216ddbe19502600d0b18932e84872b73e28
wannabeCitizen <doorsofskirpan@gmail.com>
2015-09-29 01:03:49 -0400

    all scapers are ready to scrape

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ import os
 from app.google_auth import GoogleAuth
 from app.facebook_auth import FacebookAuth
 from app.spotify_auth import SpotifyAuth
+from app.twitter_auth import TwitterAuth
+from app.reddit_auth import RedditAuth
 from app.creds import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET, REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET
 
 #Set basic options
@@ -45,7 +47,7 @@ if __name__ == "__main__":
         google_oauth =   { "key": GOOGLE_CLIENT_ID,    "secret": GOOGLE_CLIENT_SECRET   },
         facebook_oauth = { "key": FACEBOOK_CLIENT_ID,  "secret": FACEBOOK_CLIENT_SECRET },
         spotify_oauth =  { "key": SPOTIFY_CLIENT_ID, "secret": SPOTIFY_CLIENT_SECRET  },
-	twitter_oauth =  { "key": TWITTER_CLIENT_ID, "secret": TWITTER_CLIENT_SECRET  }
+	twitter_oauth =  { "key": TWITTER_CLIENT_ID, "secret": TWITTER_CLIENT_SECRET  },
         reddit_oauth = { "key": REDDIT_CLIENT_ID, "secret": REDDIT_CLIENT_SECRET},
         )
 
--- app/twitter_auth.py
+++ app/twitter_auth.py
@@ -8,35 +8,39 @@ from requests_oauthlib import OAuth1
 from urlparse import parse_qs
 
 
-class TwitterAuth(web.RequestHandler, auth.TwitterMixin):
+class TwitterAuth(web.RequestHandler):
     @web.asynchronous
     @gen.coroutine
     def get(self):
-		consumer_key = self.application.settings['twitter_oauth']['key']
-		consumer_secret = self.application.settings['twitter_oauth']['secret']
+        consumer_key = self.application.settings['twitter_oauth']['key']
+        consumer_secret = self.application.settings['twitter_oauth']['secret']
         if self.get_argument('error', None):
             raise web.HTTPError(
                     '500',
                     'Error: {0}\nReason: {1}\nDescription: {2}'.format(self.get_argument('error'), self.get_argument('error_reason','na'), self.get_argument('error_description', 'na'))
-                    )
-        if self.get_argument('code', None): #access token
-			oauth = OAuth1(consumer_key,
-				consumer_secret,
-				request_key,
-				request_secret,
-				verifier=self.get_argument('code', None))
-			r = requests.post(url='https://api.twitter.com/oauth/access_token', auth=oauth)
-			credentials = parse_qs(r.content)
-			access_token_key = credentials.get('oauth_token')[0]
-			access_token_secret = credentials.get('oauth_token_secret')[0]
+        )
+        if self.get_argument('oauth_verifier', None): #access token
+	    oauth = OAuth1(consumer_key,
+                    consumer_secret,
+                    self.get_argument('oauth_token', None),
+                    verifier=self.get_argument('oauth_verifier', None))
+            #Make async
+            r = requests.post(url='https://api.twitter.com/oauth/access_token', auth=oauth)
+	    credentials = parse_qs(r.content)
+            print "\n\ncreds:"
+            print credentials
+	    access_token_key = credentials.get('oauth_token')[0]
+	    access_token_secret = credentials.get('oauth_token_secret')[0]
+            self.redirect('https://iamadatapoint.com/test')
             return
 			
         else:# request token
-			oauth = OAuth1(consumer_key, consumer_secret)
-			r = requests.post(
-			url='https://api.twitter.com/oauth/request_token',auth=oauth)
-			credentials = parse_qs(r.content)
-			request_key = credentials.get('oauth_token')[0]
-			request_secret = credentials.get('oauth_token_secret')[0]
-			self.redirect('https://api.twitter.com/oauth/authorize?oauth_token=%s'% request_key)
+	    oauth = OAuth1(consumer_key, consumer_secret)
+	    #Make async
+            r = requests.post(
+	    url='https://api.twitter.com/oauth/request_token',auth=oauth)
+	    credentials = parse_qs(r.content)
+	    request_key = credentials.get('oauth_token')[0]
+	    request_secret = credentials.get('oauth_token_secret')[0]
+	    self.redirect('https://api.twitter.com/oauth/authorize?oauth_token={0}&oauth_secret={1}'.format(request_key, request_secret))
             return

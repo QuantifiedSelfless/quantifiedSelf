9b8e81ea8a9f8014b17764cfdbe8a527d4b59f21
wannabeCitizen <doorsofskirpan@gmail.com>
2015-10-19 22:22:25 -0400

    added leif's chatroom

--- chatroom/chatroom.js
+++
@@ -1,7 +0,0 @@
-var ws = new WebSocket("ws://iamadatapoint:8085/websocket");
- ws.onopen = function() {
-        ws.send("Hello, world");
- };
-ws.onmessage = function (evt) {
-       alert(evt.data);
-};

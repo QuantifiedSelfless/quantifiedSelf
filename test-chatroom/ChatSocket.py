05203b28ad3edf0053f5d8c60a7e7893bcba1597
tree-waldo <leif.waldner@colorado.edu>
2015-11-03 10:51:49 -0700

    added rooms, needs testing

--- test-chatroom/ChatSocket.py
+++ test-chatroom/ChatSocket.py
@@ -3,9 +3,13 @@ from tornado import websocket
 from tornado import ioloop
 from tornado import web
 from tornado import escape
+from nltk import chat
 #from chatterbot import ChatBot
 import json
-clients = []
+from room import Room
+#clients = []
+rooms = []
+chatbots = [chat.eliza]
 #bot = ChatBot("ChatBot", database="../database.db", logic_adapter="chatterbot.adapters.logic.ClosestMeaningAdapter")
 
 class WSHandler(websocket.WebSocketHandler):
@@ -13,7 +17,22 @@ class WSHandler(websocket.WebSocketHandler):
         print("open", "WebSocketChatHandler")
         #bot.train("chatterbot.corpus.english.greetings")
         #bot.train("chatterbot.corpus.english.conversations")
-        clients.append(self)
+        if (len(rooms) == 0):
+            new_room = Room(2)
+            self.current_room = new_room
+            rooms.append(new_room)
+            print("created new room")
+        for room in rooms:
+            if (room.status):
+                self.current_room = room
+                room.add_person(self)
+                return
+        print("created new room")
+        self.current_room = Room(2)
+        self.current_room.add_person(self)
+        rooms.append(self.current_room)
+
+        #clients.append(self)
     def on_message(self, json_object):
         data = escape.json_decode(json_object)
         message = data["message"]
@@ -22,14 +41,17 @@ class WSHandler(websocket.WebSocketHandler):
         else:
             #bot.train([message])
             #response = str(bot.get_response(message))
-            for client in clients:
+            #for client in clients:
+            print(len(self.current_room.people))
+            for person in self.current_room.people:
                 if (data["function"] == "elizachat"):
-                    client.write_message("Elizabot: " + message)
+                    person.write_message("Elizabot: " + message)
                 else:
-                    client.write_message(client.username + ': ' + message)
+                    person.write_message(self.username + ': ' + message)
                     #client.write_message("ChatBot: " + response)
     def on_close(self):
-        clients.remove(self)
+        #self.current_room.remove_person
+        self.current_room.remove_person(self)
 
 class IndexHandler(web.RequestHandler):
     @web.asynchronous
--- test-chatroom/room.py
+++ test-chatroom/room.py
@@ -1,26 +1,15 @@
 class Room():
-    def __init__(self, name, ID, owner):
-        self.name = name
-        self.ID = ID
-        self.owner = owner
+    def __init__(self, peoplelimit):
         self.people = []
-        self.peoplelimit = 6
-        self.status = "available"
-        self.private = false
+        self.peoplelimit = peoplelimit
+        self.status = True
     def add_person(self, personID):
-        if (self.status == "available"):
-            self.people.append(personID)
-    def remove_person(self, personID)
-        for i in range(len(self.people)):
-            if (self.people[i] == personID)
-                self.people[i] = None
-    def get_Person(self, personID):
+        self.people.append(personID)
+        if (len(self.people) == self.peoplelimit):
+            self.status = False    
+    def remove_person(self, personID):
         for i in range(len(self.people)):
             if (self.people[i] == personID):
                 self.people[i] = None
-    def is_available(self):
-        return (self.status == "available")
-    def is_private(self):
-        return self.private
-
-#module.exports = Room
+    def status(self):
+        return self.status

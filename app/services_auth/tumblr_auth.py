eff6bbfba3420daaac8acddd02c398ddd26c99e1
Micha Gorelick <mynameisfiber@gmail.com>
2016-02-05 15:37:07 -0500

    fixed oauth bugs including one caused by instagram's incompetance

--- app/services_auth/tumblr_auth.py
+++ app/services_auth/tumblr_auth.py
@@ -34,7 +34,7 @@ class TumblrAuth(OAuthRequestHandler):
 
     def startFlow(self):
         resp, content = self.client.request(self.request_token_url, "POST")
-        request_token = urllib.parse.parse_qs(content)
+        request_token = urllib.parse.parse_qs(content.decode())
         auth_session_id = self.auth_session_id()
         secrets[auth_session_id] = request_token['oauth_token_secret'][0]
         self.set_secure_cookie("auth-session-id", auth_session_id)

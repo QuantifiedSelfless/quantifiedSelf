2ee338d16f5a12858bea310dd180bbc4ad50818d
wannabeCitizen <doorsofskirpan@gmail.com>
2016-02-25 13:43:26 -0500

    fixed up the thank you page after RSVP and edited our confirmation email to include account deauth

--- web/js/signup_comps/Thanks.js
+++ web/js/signup_comps/Thanks.js
@@ -1,12 +1,8 @@
 var Thanks = React.createClass({
 
     getInitialState: function() {
-        // Check cookie
-        var authed = false;
 
-
-        return {
-            ticket: 'just a second...'
+        return {    
         };
     },
 
@@ -15,8 +11,8 @@ var Thanks = React.createClass({
             <div className="clearfix white" id="thankyou">
                 <div className="col-10 mx-auto">
                     <h2>Thank you!</h2>
-                    <p>Your ticket number is {this.state.ticket}</p>
-                    <p>We will be emailing you about your ticket shortly. We will also send you a reminder email before the show and double check you can still make it.</p>
+                    <p>Your ticketing confirmation number is being sent to your email address.</p>
+                    <p>We will send a reminder email to you about your reservations a week or so before the show. If you do not receive a confirmation email, please check spam to be sure you are receiving our emails properly.</p>
                     <p>Since we have such limited space, please cancel with us if you realize you cannot make it. You can cancel your reservation and delete your account at <a href="/leave">www.iamadatapoint.com/leave</a></p>
                 </div>
             </div>
@@ -26,4 +22,3 @@ var Thanks = React.createClass({
 });
 
 module.exports = Thanks;
-//May want a handleclick function
